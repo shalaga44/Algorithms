59357b2fb4beb507989c1bb3afc5142f8a469061
Mohamed Moawia <mohamedmoawia4@gmail.com>
2020-08-02 06:09:38 +0200

    add BinaryHeap and its test

--- src/main/kotlin/com/shalaga44/algorithms/datassructures/priorityqueue/BinaryHeap.kt
+++ src/main/kotlin/com/shalaga44/algorithms/datassructures/priorityqueue/BinaryHeap.kt
@@ -1,7 +1,134 @@
 package com.shalaga44.algorithms.datassructures.priorityqueue
 
 
-class BinaryHeap {
+class BinaryHeap<T : Comparable<T>> {
+    private var heapSize = 0
+
+    private var heapCapacity = 0
+
+    private var heap: MutableList<T> = mutableListOf()
 
 
+    fun isEmpty(): Boolean {
+        return heapSize == 0
+    }
+
+    fun size(): Int {
+        return heapSize
+    }
+
+    fun clear() {
+        heap.clear()
+        heapSize = 0
+    }
+
+    fun peek(): T? {
+        return when {
+            isEmpty() -> null
+            else -> heap[0]
+        }
+    }
+
+    fun poll(): T? {
+        return removeAt(0)
+    }
+
+    operator fun contains(elem: T): Boolean {
+        for (i in 0 until heapSize)
+            if (heap[i] == elem)
+                return true
+        return false
+    }
+
+    fun add(elem: T?) {
+        requireNotNull(elem)
+        if (heapSize < heapCapacity) {
+            heap[heapSize] = elem
+        } else {
+            heap.add(elem)
+            heapCapacity++
+        }
+        swim(heapSize)
+        heapSize++
+    }
+
+    private fun less(i: Int, j: Int): Boolean {
+        val node1 = heap[i]
+        val node2 = heap[j]
+        return node1 <= node2
+    }
+
+
+    private fun swim(node: Int) {
+
+        var k = node
+        var parent = (k - 1) / 2
+
+        while (k > 0 && less(k, parent)) {
+            swap(parent, k)
+            k = parent
+
+            parent = (k - 1) / 2
+        }
+    }
+
+    private fun sink(node: Int) {
+        var k = node
+        while (true) {
+            val left = 2 * k + 1 // Left  node
+            val right = 2 * k + 2 // Right node
+            var smallest = left // Assume left is the smallest node of the two children
+
+            // Find which is smaller left or right
+            // If right is smaller set smallest to be right
+            if (right < heapSize && less(right, left)) smallest = right
+
+            // Stop if we're outside the bounds of the tree
+            // or stop early if we cannot sink k anymore
+            if (left >= heapSize || less(k, smallest)) break
+
+            // Move down the tree following the smallest node
+            swap(smallest, k)
+            k = smallest
+        }
+    }
+
+    private fun swap(i: Int, j: Int) {
+        val elemI = heap[i]
+        val elemJ = heap[j]
+        heap[i] = elemJ
+        heap[j] = elemI
+    }
+
+    fun remove(element: T?): Boolean {
+        if (element == null) return false
+        for (i in 0 until heapSize) {
+            if (element == heap[i]) {
+                removeAt(i)
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun removeAt(i: Int): T? {
+        if (isEmpty()) return null
+        heapSize--
+        val removedData = heap[i]
+        swap(i, heapSize)
+
+        heap.removeAt(heapSize)
+
+        if (i == heapSize) return removedData
+        val elem = heap[i]
+
+        sink(i)
+
+        if (heap[i] == elem) swim(i)
+        return removedData
+    }
+
+    override fun toString(): String {
+        return heap.toString()
+    }
 }
--- src/test/kotlin/com/shalaga44/algorithms/datassructures/priorityqueue/BinaryHeapTest.kt
+++ src/test/kotlin/com/shalaga44/algorithms/datassructures/priorityqueue/BinaryHeapTest.kt
@@ -1,12 +1,144 @@
 package com.shalaga44.algorithms.datassructures.priorityqueue
 
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import kotlin.properties.Delegates
 
 class BinaryHeapTest {
+    private lateinit var pQueue: BinaryHeap<Int>
+    private var intItem by Delegates.notNull<Int>()
+    private lateinit var sortedIntegerItems: List<Int>
+    private lateinit var randomIntegerList: List<Int>
+
+    @BeforeEach
+    fun `Create Empty Priority Queue`() {
+        pQueue = BinaryHeap()
+        intItem = (0..100).random()
+        sortedIntegerItems = (0..5).toList()
+        randomIntegerList = (0..20).shuffled()
+
+    }
 
     @Test
-    fun test() {
-        assertEquals(1, 1)
+    fun `isEmpty should return true for empty heap`() {
+        assertTrue(pQueue.isEmpty())
+    }
+
+    @Test
+    fun `Adding null item should throw the IllegalArgumentException`() {
+        val exception = assertThrows(IllegalArgumentException::class.java) {
+            pQueue.add(null)
+        }
+        assertEquals("Required value was null.", exception.localizedMessage)
+    }
+
+    @Test
+    fun `Removing item when heap isEmpty should return false`() {
+        val result = pQueue.remove(0)
+        assertFalse(result)
+    }
+
+    @Test
+    fun `Poll item when heap isEmpty should return null`() {
+        val item = pQueue.poll()
+        assertNull(item)
+    }
+
+    @Test
+    fun `Add one item should peek it twice `() {
+        pQueue.add(intItem)
+        assertEquals(intItem, pQueue.peek())
+        assertEquals(intItem, pQueue.peek())
+    }
+
+    @Test
+    fun `Add one item and poll it should peek null`() {
+        pQueue.add(intItem)
+        assertEquals(intItem, pQueue.poll())
+        assertNull(pQueue.peek())
+    }
+
+    @Test
+    fun `isEmpty should return true after one add end one poll`() {
+        pQueue.add(intItem)
+        assertEquals(intItem, pQueue.poll())
+        assertTrue(pQueue.isEmpty())
+    }
+
+    @Test
+    fun `Add sorted items should in order poll them`() {
+        sortedIntegerItems.forEach { item ->
+            pQueue.add(item)
+        }
+
+        sortedIntegerItems.forEach { item ->
+            assertEquals(item, pQueue.poll())
+        }
+    }
+
+    @Test
+    fun `Add shuffled items should first poll the smallest`() {
+        randomIntegerList.forEach { item ->
+            pQueue.add(item)
+        }
+        assertEquals(randomIntegerList.min(), pQueue.poll())
+    }
+
+    @Test
+    fun `Add shuffled items should poll the smallest every time`() {
+        randomIntegerList.forEach { item ->
+            pQueue.add(item)
+        }
+        randomIntegerList.sorted().forEach { item ->
+            assertEquals(item, pQueue.poll())
+        }
+    }
+
+    @Test
+    fun `Adding shuffled items then remove them should make heap empty`() {
+        randomIntegerList.forEach { item ->
+            pQueue.add(item)
+        }
+        randomIntegerList.forEach { item ->
+            assertTrue(pQueue.remove(item))
+        }
+        assertTrue(pQueue.isEmpty())
+    }
+
+    @Test
+    fun `Heap size should be incremented when adding `() {
+        randomIntegerList.forEachIndexed { index, item ->
+            pQueue.add(item)
+            assertEquals(index + 1, pQueue.size())
+
+        }
+    }
+
+    @Test
+    fun `Heap size should be decremented when poll `() {
+        randomIntegerList.forEach { item -> pQueue.add(item) }
+        randomIntegerList.forEachIndexed { index, _ ->
+            pQueue.poll()
+            assertEquals(randomIntegerList.lastIndex - index, pQueue.size())
+
+        }
+    }
+
+    @Test
+    fun `Heap size should be decremented when removing `() {
+        randomIntegerList.forEach { item -> pQueue.add(item) }
+        randomIntegerList.forEachIndexed { index, item ->
+            pQueue.remove(item)
+            assertEquals(randomIntegerList.lastIndex - index, pQueue.size())
+
+        }
+    }
+
+    @Test
+    fun `After clearing the heap poll returns null`() {
+        randomIntegerList.forEach { item -> pQueue.add(item) }
+        pQueue.clear()
+        assertNull(pQueue.poll())
     }
 }
